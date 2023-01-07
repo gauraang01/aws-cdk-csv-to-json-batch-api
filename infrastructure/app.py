1ac89195ba7b218937c42b517ce10252982b43c9
Gauraang <zigbee.sh@gmail.com>
2023-01-07 19:11:28 +0530

    add RDS IAAC

--- infrastructure/app.py
+++ infrastructure/app.py
@@ -3,11 +3,11 @@ import os
 
 import aws_cdk as cdk
 
-from lambda_s3_trigger.lambda_s3_trigger_stack import LambdaS3TriggerStack
+from lambda_s3_trigger.lambda_s3_trigger_stack import LambdaS3TriggerRDSStack
 
 
 app = cdk.App()
-LambdaS3TriggerStack(app, "LambdaS3TriggerStack",
+LambdaS3TriggerRDSStack(app, "LambdaS3TriggerRDSStack",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
@@ -15,7 +15,7 @@ LambdaS3TriggerStack(app, "LambdaS3TriggerStack",
     # Uncomment the next line to specialize this stack for the AWS Account
     # and Region that are implied by the current CLI configuration.
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+    env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
 
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
--- infrastructure/lambda_s3_trigger/lambda_s3_trigger_stack.py
+++ infrastructure/lambda_s3_trigger/lambda_s3_trigger_stack.py
@@ -1,17 +1,49 @@
 from aws_cdk import (
     aws_lambda as _lambda,
+    aws_rds as rds,
     aws_s3 as _s3,
     aws_s3_notifications,
+    aws_ec2 as ec2,
     Duration,
     Stack
 )
 
 from constructs import Construct
 
-class LambdaS3TriggerStack(Stack):
+class LambdaS3TriggerRDSStack(Stack):
 
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
+
+        # Retrieve the default VPC for the account
+        vpc = ec2.Vpc.from_lookup(self, "MyVPC", is_default=True)
+        
+        sg = ec2.SecurityGroup(self, 'mydb-sg',
+            vpc=vpc,
+            allow_all_outbound=True,
+        )
+
+        # Allow inbound traffic on ports 3306, 80, and 443
+        sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(3306))
+        sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(80))
+        sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(443))
+
+        # Create an Amazon RDS for MySQL database
+        db = rds.DatabaseInstance(
+            self, "mydb",
+            engine=rds.DatabaseInstanceEngine.MYSQL,
+            instance_type=ec2.InstanceType.of(
+                instance_class=ec2.InstanceClass.T3,
+                instance_size=ec2.InstanceSize.MICRO
+            ),
+            vpc = vpc, 
+             vpc_subnets=ec2.SubnetSelection(
+                subnet_type=ec2.SubnetType.PUBLIC,
+            ),
+            security_groups=[sg],
+            database_name='mydb'
+        )
+
 
         # create s3 bucket
         s3 = _s3.Bucket(self, "s3bucket")
